6a654898abba18c643da6d92cf9bfa54ccef87cf
vadims lubimovs <vadims.lubimovs@n-fuse.co>
2018-09-28 18:15:24 +0300

    hello

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Hero } from './../hero';
+import { HEROES } from './../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes  = HEROES;
+  selectedHero: Hero;
+
+
+  constructor() { }
+
+  ngOnInit() {
+    this.heroes = HEROES;
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+}
