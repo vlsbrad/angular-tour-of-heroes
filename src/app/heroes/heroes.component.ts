1561f76542e651c4b5b6ddadd2bccb6339e80d37
vadims lubimovs <vadims.lubimovs@n-fuse.co>
2018-10-03 11:51:01 +0300

    finishing routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,7 +18,11 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
+
+    // this.heroService.getHeroes()
+    //   .subscribe(heroes => this.heroes = heroes);
+
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
